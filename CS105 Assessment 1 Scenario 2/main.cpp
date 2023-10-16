139e1ae829d9a163d9ae696d6ac56ba8c3e3ef33
Matt T <mrsymtec@gmail.com>
2023-10-17 10:03:14 +1300

    Continuing to edit the output of the characters created and their attacks

--- CS105 Assessment 1 Scenario 2/main.cpp
+++ CS105 Assessment 1 Scenario 2/main.cpp
@@ -1,39 +1,143 @@
-// CS105 - RPG-Game-Scenario2.cpp
-// Matt Taylor 
+// Scenario 2
+// Student Name: Matt Taylor
+// This is a program that creates three character players as outlined in the brief
 
-// main.cpp
 #include <iostream>
 #include <vector>
-#include "player.h"
-#include "warrior.h"
-#include "priest.h"
-#include "mage.h"
+
+enum Race { HUMAN, ELF, DWARF, ORC, TROLL };
+//This function converts the enum values to corresponding string reperesentations
+std::string raceToString(Race race) {
+    switch (race) {
+    case HUMAN:
+        return "HUMAN";
+    case ELF:
+        return "ELF";
+    case DWARF:
+        return "DWARF";
+    case ORC:
+        return "ORC";
+    case TROLL:
+        return "TROLL";
+    default:
+        return "UNKNOWN";
+    }
+};
+
+//This is the base class. 
+class Player {
+private:
+    std::string name;
+    Race race;
+    int hitPoints;
+    int magicPoints;
+
+public:
+    Player(std::string name, Race race, int hitPoints, int magicPoints)
+        : name(name), race(race), hitPoints(hitPoints), magicPoints(magicPoints) {}
+
+    std::string getName() const {
+        return name;
+    }
+
+    Race getRace() const {
+        return race;
+    }
+
+    int getHitPoints() const {
+        return hitPoints;
+    }
+
+    int getMagicPoints() const {
+        return magicPoints;
+    }
+
+    void setName(std::string name) {
+        this->name = name;
+    }
+
+    void setRace(Race race) {
+        this->race = race;
+    }
+
+    void setHitPoints(int hitPoints) {
+        this->hitPoints = hitPoints;
+    }
+
+    void setMagicPoints(int magicPoints) {
+        this->magicPoints = magicPoints;
+    }
+
+    virtual std::string attack() const {
+        return "No attack method defined yet!";
+    }
+
+    virtual std::string getClass() const {
+        return "Player";
+    }
+};
+//below are the derived classes for each class. 
+class Warrior : public Player {
+public:
+    Warrior(std::string name, Race race)
+        : Player(name, race, 200, 0) {}
+
+    std::string attack() const override {
+        return "I will destroy you with my sword, foul demon!";
+    }
+
+    std::string getClass() const override {
+        return "Warrior";
+    }
+
+};
+
+class Priest : public Player {
+public:
+    Priest(std::string name, Race race)
+        : Player(name, race, 100, 200) {}
+
+    std::string attack() const override {
+        return "I will assault you with holy wrath!";
+    }
+
+    std::string getClass() const override {
+        return "Priest";
+    }
+};
+
+class Mage : public Player {
+public:
+    Mage(std::string name, Race race)
+        : Player(name, race, 200, 0) {}
+
+    std::string attack() const override {
+        return "I will crush you with my arcane missiles!";
+    }
+
+    std::string getClass() const override {
+        return "Mage";
+    }
+};
 
 int main() {
-    std::vector<Warrior> warriors;
-    std::vector<Priest> priests;
-    std::vector<Mage> mages;
 
-    while (true) {
-        std::cout << "Choose a class for your player:" << std::endl;
-        std::cout << "1. Warrior" << std::endl;
-        std::cout << "2. Priest" << std::endl;
-        std::cout << "3. Mage" << std::endl;
-        std::cout << "4. Exit" << std::endl;
-        int choice;
-        std::cin >> choice;
+std::cout << "******Character Creation******\n";
+std::cout << "" << std::endl;
 
-        if (choice == 4) {
-            break;
-        }
+    std::vector<Player*> players;
+    const int maxPlayers = 3; // Allow the creation of three characters
+    // This is the loop for creating the characters. 
+    for (int i = 0; i < maxPlayers; ++i) {
+        std::cout << "Create character " << (i + 1) << ":" << std::endl;
 
         std::string name;
-        std::cout << "Enter player name: ";
+        std::cout << "What would you like to name your character?: ";
         std::cin >> name;
 
         Race race;
         int raceChoice;
-        std::cout << "Choose a race for your player:" << std::endl;
+        std::cout << "What race do you want?: " << std::endl;
         std::cout << "1. HUMAN" << std::endl;
         std::cout << "2. ELF" << std::endl;
         std::cout << "3. DWARF" << std::endl;
@@ -49,40 +153,41 @@ int main() {
         default: race = HUMAN; break;
         }
 
-        switch (choice) {
+        int classChoice;
+        std::cout << "Choose a class for your character: " << std::endl;
+        std::cout << "1. Create a Warrior" << std::endl;
+        std::cout << "2. Create a Priest" << std::endl;
+        std::cout << "3. Create a Mage" << std::endl;
+        std::cin >> classChoice;
+
+        Player* player = nullptr;
+
+        switch (classChoice) {
         case 1:
-            warriors.emplace_back(name, race);
+            player = new Warrior(name, race);
             break;
         case 2:
-            priests.emplace_back(name, race);
+            player = new Priest(name, race);
             break;
         case 3:
-            mages.emplace_back(name, race);
+            player = new Mage(name, race);
             break;
         default:
-            std::cout << "Invalid choice. Please try again." << std::endl;
-            break;
+            std::cout << "Invalid choice. Exiting." << std::endl;
+            return 0;
         }
+
+        players.push_back(player);
+
     }
 
-    // Print player details and attack methods
-    for (const Warrior& warrior : warriors) {
-        std::cout << "Name: " << warrior.getName() << ", Race: " << warrior.getRace() << ", HP: "
-            << warrior.getHitPoints() << ", MP: " << warrior.getMagicPoints()
-            << ", Attack: " << warrior.attack() << std::endl;
+    // Print player details, including class and race
+    for (Player* player : players) {
+        std::cout << "My name is " << player->getName() << ", I am a  " << raceToString(player->getRace()) << ", I am a  "
+            << player->getClass() << ", and my attack is " << player->attack() << std::endl;
+        delete player; // Deallocate memory for each player
     }
 
-    for (const Priest& priest : priests) {
-        std::cout << "Name: " << priest.getName() << ", Race: " << priest.getRace() << ", HP: "
-            << priest.getHitPoints() << ", MP: " << priest.getMagicPoints()
-            << ", Attack: " << priest.attack() << std::endl;
-    }
-
-    for (const Mage& mage : mages) {
-        std::cout << "Name: " << mage.getName() << ", Race: " << mage.getRace() << ", HP: "
-            << mage.getHitPoints() << ", MP: " << mage.getMagicPoints()
-            << ", Attack: " << mage.attack() << std::endl;
-    }
 
     return 0;
 }
